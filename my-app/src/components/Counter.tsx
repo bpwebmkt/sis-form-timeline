a745eee0a40c32b00b6e78bfda0b3b965cf06ea9
mktdigbp <118197705+bpwebmkt@users.noreply.github.com>
2024-05-10 17:45:52 -0300

    stre

--- my-app/src/components/Counter.tsx
+++ my-app/src/components/Counter.tsx
@@ -1,7 +1,11 @@
 import { useEffect, useState } from "react";
 
 const Counter = () => {
-  const [currentTime, setCurrentTime] = useState(new Date());
+  // const [currentTime, setCurrentTime] = useState(new Date());
+
+
+  const FULLTIME = 5;
+  const timelineTray = [];
   const events = [
     {
       id: 1,
@@ -11,79 +15,69 @@ const Counter = () => {
       id: 2,
       name: "2b",
     },
-    {
-      id: 3,
-      name: "1a",
-    },
-    {
-      id: 4,
-      name: "2b",
-    },
-    {
-      id: 5,
-      name: "1a",
-    },
-    {
-      id: 1,
-      name: "1a",
-    },
-    {
-      id: 2,
-      name: "2b",
-    },
-    {
-      id: 3,
-      name: "1a",
-    },
-    {
-      id: 4,
-      name: "2b",
-    },
-    {
-      id: 5,
-      name: "1a",
-    },
+
   ];
 
-  const fullTimer = 30;
+  // events 
   const totalEvents =
-    events.length <= fullTimer / 2 ? events.length : fullTimer / 2;
+    events.length <= FULLTIME / 2 ? events.length : FULLTIME / 2;
 
-  // create tray
-  const gapBetweenEvents = fullTimer / totalEvents;
+  // gap between events
+  const gapBetweenEvents = Math.floor(FULLTIME / totalEvents);
 
   let currentEvent = 0;
   let count = 0;
-  for (let i = 0; i < fullTimer; i++) {
+
+
+  for (let i = 0; i < FULLTIME; i++) {
     count += 1;
+    let message = "";
 
     if (count === gapBetweenEvents) {
       count = 0;
       const hasEventsLeft = currentEvent < totalEvents;
+
       if (hasEventsLeft) {
         currentEvent += 1;
         console.log("event:", currentEvent);
+        message = "event";
       } else {
         console.log("has no event");
+        message = "has no event";
       }
+    
     } else {
       console.log("pass", i);
+        message = "pass";
     }
+
+    timelineTray.push({
+      id: i,
+      message: message
+    })
+
   }
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setCurrentTime(new Date());
-    }, 1000); // Update every second
 
-    return () => {
-      clearInterval(timer); // Clean up the timer on component unmount
-    };
-  }, []); // Empty dependency array to run the effect only once on component mount
+  console.log("tray", timelineTray)
+
+
+
+
+
+  // useEffect(() => {
+  //   const timer = setInterval(() => {
+  //     setCurrentTime(new Date());
+  //   }, 1000); // Update every second
+
+  //   return () => {
+  //     clearInterval(timer); // Clean up the timer on component unmount
+  //   };
+  // }, []); // Empty dependency array to run the effect only once on component mount
   return (
     <div className="border">
       <h1>Counter</h1>
-      Current Time: {currentTime.toLocaleTimeString()}
+      {/*Current Time: {currentTime.toLocaleTimeString()}*/}
     </div>
   );
 };
